ccf34b803bdf816ccd35388485793a06d628c64e
irania90 <wwwwwq37@gmail.com>
2022-08-10 16:27:12 +0430

    handling if slug dosnt exist.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 def home(request):
@@ -9,6 +9,6 @@ def home(request):
 
 def detail(request, slug):
     context = {
-        "article": Article.objects.get(slug= slug)
+        "article": get_object_or_404(Article, slug=slug, status='p')
     }
     return render(request, "blog/detail.html", context)
