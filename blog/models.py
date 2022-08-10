9a782e6f166578aa2dddb9ce80f7f79b918d8dc7
irania90 <wwwwwq37@gmail.com>
2022-08-10 16:37:40 +0430

    Persianization of the management menu and set time zone Asia/Tehran.

--- blog/models.py
+++ blog/models.py
@@ -3,17 +3,21 @@ from django.utils import timezone
 
 class Article(models.Model):
     STATUS_CHOICES = (
-        ('d', 'Draft'),
-        ('p', 'Publish'),
+        ('d', 'پیش‌نویس'),
+        ('p', 'منتشر شده'),
     )
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to="images")
-    publish = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=200, verbose_name="عنوان مقاله")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس مقاله")
+    description = models.TextField(verbose_name="محتوا")
+    thumbnail = models.ImageField(upload_to="images", verbose_name="تصویر مقاله")
+    publish = models.DateTimeField(default=timezone.now, verbose_name="زمان انتشار")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name="وضعیت")
+
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقالات"
 
     def __str__(self) -> str:
         return self.title
