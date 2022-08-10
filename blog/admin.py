f4eb06b09bc6ace310882540a92179d1a681e5c3
irania90 <wwwwwq37@gmail.com>
2022-08-10 18:08:57 +0430

    add catagory for articles.

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Catagory
+
+class CatagoryAdmin(admin.ModelAdmin):
+    list_display = ['position', 'title', 'slug', 'status'] 
+    list_filter = ['status']
+    search_fields = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)} 
+    
+admin.site.register(Catagory, CatagoryAdmin)
+
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'jpublish', 'status'] 
+    list_display = ['title', 'slug', 'jpublish', 'status', 'catagoy_to_list'] 
     list_filter = ['publish', 'status']
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['-status', 'publish']
     
+    def catagoy_to_list(self, obj):
+        catagoties = []
+        for catagory in obj.catagory.all():
+            catagoties.append(catagory.title)
+        
+        if len(catagoties) == 0:
+            return "بدون دسته بندی"
+        else:
+            return "، ".join(catagoties)
+
+    catagoy_to_list.short_description = "دسته بندی"
+
+
 admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.utils import timezone
 from extentions.utils import jalali_converter
 
+class Catagory(models.Model):
+    title = models.CharField(max_length=200, verbose_name="عنوان دسته بندی")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس دسته بندی")
+    status = models.BooleanField(default=True, verbose_name="آیا نمایش داده شود؟")
+    position = models.IntegerField(verbose_name="پوزیشن")
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"   
+        ordering = ['position']
+
+    def __str__(self) -> str:
+        return self.title
 class Article(models.Model):
     STATUS_CHOICES = (
         ('d', 'پیش‌نویس'),
@@ -9,6 +21,7 @@ class Article(models.Model):
     )
     title = models.CharField(max_length=200, verbose_name="عنوان مقاله")
     slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس مقاله")
+    catagory = models.ManyToManyField(Catagory, verbose_name="دسته بندی")
     description = models.TextField(verbose_name="محتوا")
     thumbnail = models.ImageField(upload_to="images", verbose_name="تصویر مقاله")
     publish = models.DateTimeField(default=timezone.now, verbose_name="زمان انتشار")
@@ -27,3 +40,4 @@ class Article(models.Model):
         return jalali_converter(self.publish)
 
     jpublish.short_description = "زمان انتشار"
+
