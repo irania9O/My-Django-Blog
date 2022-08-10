a642443d8cee108f621f0fb159e4b94eec317d42
irania90 <wwwwwq37@gmail.com>
2022-08-10 13:56:21 +0430

    config Artcle in admin panel.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'publish', 'status'] 
+    list_filter = ['publish', 'status']
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['-status', 'publish']
+    
+admin.site.register(Article, ArticleAdmin)
