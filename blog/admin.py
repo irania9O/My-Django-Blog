9af90d9b079ca7edf11630e521499f9dc3748a8e
irania90 <wwwwwq37@gmail.com>
2022-08-11 22:45:40 +0430

    add action admin to articles and categories.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 from .models import Article, Catagory, SiteSetting
 
 
@@ -21,23 +22,61 @@ class SiteSettingsAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
  
- 
 admin.site.register(SiteSetting, SiteSettingsAdmin)
+
+#-------------------------------------------------------------
+@admin.action(description='فعال کردن دسته بندی های انتخاب شده')
+def make_active(modeladmin, request, queryset):
+    updated = queryset.update(status=True)
+    modeladmin.message_user(request, ngettext(
+                '%d دسته بندی فعال شد.',
+                '%d دسته بندی ها فعال شدند.',
+                updated,
+            ) % updated, messages.SUCCESS)
+
+@admin.action(description='غیر فعال کردن دسته بندی های انتخاب شده')
+def make_diactive(modeladmin, request, queryset):
+    updated = queryset.update(status=False)
+    modeladmin.message_user(request, ngettext(
+                '%d دسته بندی  غیر فعال شد.',
+                '%d دسته بندی ها غیر فعال شدند.',
+                updated,
+            ) % updated, messages.SUCCESS)
 class CatagoryAdmin(admin.ModelAdmin):
     list_display = ['position', 'title', 'parent', 'slug', 'status'] 
     list_filter = ['status']
     search_fields = ['title', 'slug']
     prepopulated_fields = {'slug': ('title',)} 
+    actions = [make_active, make_diactive]
     
 admin.site.register(Catagory, CatagoryAdmin)
 
+#-------------------------------------------------------------
+@admin.action(description='انتشار مقالات انتخاب شده')
+def make_published(modeladmin, request, queryset):
+    updated = queryset.update(status='p')
+    modeladmin.message_user(request, ngettext(
+                '%d مقاله منتشر شد.',
+                '%d مقاله منتشر شدند.',
+                updated,
+            ) % updated, messages.SUCCESS)
 
+@admin.action(description='پیش نویس شدن مقالات انتخاب شده')
+def make_draft(modeladmin, request, queryset):
+    updated = queryset.update(status='d')
+    modeladmin.message_user(request, ngettext(
+                '%d مقاله پیش‌نویس شد.',
+                '%d مقاله پیش‌نویس شدند.',
+                updated,
+            ) % updated, messages.SUCCESS)
+            
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'jpublish', 'status', 'catagoy_to_list'] 
     list_filter = ['publish', 'status']
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['-status', 'publish']
+    actions = [make_published, make_draft]
     
     def catagoy_to_list(self, obj):
         catagoties = []
@@ -50,6 +89,5 @@ class ArticleAdmin(admin.ModelAdmin):
             return "، ".join(catagoties)
 
     catagoy_to_list.short_description = "دسته بندی"
-
 
 admin.site.register(Article, ArticleAdmin)
