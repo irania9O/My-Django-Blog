fc05c56701938570e64a62a40d7daa27956cdfa7
irania90 <wwwwwq37@gmail.com>
2022-08-11 22:20:38 +0430

    using base views instead of functions.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import home, detail, catagory
+from .views import ArticleList, ArticleDetail, CatagoryList
 
 app_name = "blog"
 urlpatterns = [
-    path('', home, name= "home"),
-    path('page/<int:page>', home, name= "home"),
-    path('article/<slug:slug>' , detail, name= "detail"),
-    path('catagory/<slug:slug>' , catagory, name= "catagory"),
-    path('catagory/<slug:slug>/page/<int:page>' , catagory, name= "catagory")
+    path('', ArticleList.as_view(), name= "home"),
+    path('page/<int:page>', ArticleList.as_view(), name= "home"),
+    path('article/<slug:slug>' , ArticleDetail.as_view(), name= "detail"),
+    path('catagory/<slug:slug>' , CatagoryList.as_view(), name= "catagory"),
+    path('catagory/<slug:slug>/page/<int:page>' , CatagoryList.as_view(), name= "catagory")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,61 @@
+from msilib.schema import Class
 from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from .models import Article, Catagory
 
-def home(request, page=1):
-    articles_list = Article.objects.published()
-    paginator = Paginator(articles_list, 5)
-    articles = paginator.get_page(page)
-    context = {
-        "articles": articles
-    }
-    return render(request, "blog/home.html", context)
+# def home(request, page=1):
+#     articles_list = Article.objects.published()
+#     paginator = Paginator(articles_list, 5)
+#     articles = paginator.get_page(page)
+#     context = {
+#         "articles": articles
+#     }
+#     return render(request, "blog/home.html", context)
 
-def detail(request, slug):
-    context = {
-        "article": get_object_or_404(Article.objects.published(), slug=slug)
-    }
-    return render(request, "blog/detail.html", context)
+class ArticleList(ListView):
+    # model = Article
+    # template_name = "blog/home.html"
+    # context_object_name = "articles"
+    queryset = Article.objects.published()
+    paginate_by = 5
 
-def catagory(request, slug, page=1):
-    catagory = get_object_or_404(Catagory, slug=slug, status=True)
-    articles_list = catagory.articles.published()
-    paginator = Paginator(articles_list, 5)
-    articles = paginator.get_page(page)
-    context = {
-        "catagory": catagory,
-        "articles": articles
-    }
-    return render(request, "blog/catagory.html", context)
+#----------------------------------------
+# def detail(request, slug):
+#     context = {
+#         "article": get_object_or_404(Article.objects.published(), slug=slug)
+#     }
+#     return render(request, "blog/detail.html", context)
+
+class ArticleDetail(DetailView):
+
+    def get_object(self):
+        slug = self.kwargs.get("slug")
+        return get_object_or_404(Article.objects.published(), slug=slug)
+
+#----------------------------------------
+# def catagory(request, slug, page=1):
+#     catagory = get_object_or_404(Catagory, slug=slug, status=True)
+#     articles_list = catagory.articles.published()
+#     paginator = Paginator(articles_list, 5)
+#     articles = paginator.get_page(page)
+#     context = {
+#         "catagory": catagory,
+#         "articles": articles
+#     }
+#     return render(request, "blog/catagory.html", context)
+
+class CatagoryList(ListView):
+    paginate_by = 5
+    template_name = "blog/catagory_list.html"
+
+    def get_queryset(self):
+        global catagory
+        slug = self.kwargs.get("slug")
+        catagory = get_object_or_404(Catagory.objects.active(), slug=slug)
+        return catagory.articles.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['catagory'] = catagory
+        return context
