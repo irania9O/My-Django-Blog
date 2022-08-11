f28e6643484f806ea06661f10f7541fe30f63cb6
irania90 <wwwwwq37@gmail.com>
2022-08-11 15:59:14 +0430

    paginator home and catagory pages.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from .views import home, detail, catagory
 app_name = "blog"
 urlpatterns = [
     path('', home, name= "home"),
+    path('page/<int:page>', home, name= "home"),
     path('article/<slug:slug>' , detail, name= "detail"),
-    path('catagory/<slug:slug>' , catagory, name= "catagory")
+    path('catagory/<slug:slug>' , catagory, name= "catagory"),
+    path('catagory/<slug:slug>/page/<int:page>' , catagory, name= "catagory")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Article, Catagory
 
-def home(request):
+def home(request, page=1):
+    articles_list = Article.objects.published()
+    paginator = Paginator(articles_list, 5)
+    articles = paginator.get_page(page)
     context = {
-        "articles": Article.objects.published()
+        "articles": articles
     }
     return render(request, "blog/home.html", context)
 
@@ -13,8 +17,13 @@ def detail(request, slug):
     }
     return render(request, "blog/detail.html", context)
 
-def catagory(request, slug):
+def catagory(request, slug, page=1):
+    catagory = get_object_or_404(Catagory, slug=slug, status=True)
+    articles_list = catagory.articles.published()
+    paginator = Paginator(articles_list, 5)
+    articles = paginator.get_page(page)
     context = {
-        "catagory": get_object_or_404(Catagory, slug=slug, status=True)
+        "catagory": catagory,
+        "articles": articles
     }
     return render(request, "blog/catagory.html", context)
