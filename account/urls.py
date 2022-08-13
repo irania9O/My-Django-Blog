fadc69e89337c31be10c831244e99cefa318c6db
irania90 <wwwwwq37@gmail.com>
2022-08-13 20:08:47 +0430

    add logout to panel admin.

--- account/urls.py
+++ account/urls.py
@@ -11,7 +11,7 @@ app_name = "account"
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 
     # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
     # path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_REDIRECT_URL = "account:home"
 LOGIN_URL = "account:login"
+LOGOUT_REDIRECT_URL = 'account:login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
