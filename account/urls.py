7c03d182a4770774b22567ad8b06b6130d6cae22
irania90 <wwwwwq37@gmail.com>
2022-08-14 12:51:07 +0430

    add change password.

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,8 @@ from .views import (
     ArticleUpdate, 
     ArticleDelete,
     Profile,
-    Login
+    Login,
+    PasswordChange
     )
 
 app_name = "account"
@@ -15,8 +16,8 @@ urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
-    # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_change/', PasswordChange.as_view(), name='password_change'),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     # path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     # path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from .forms import ProfileForm
 from .mixins import (
     FieldsMixin, 
@@ -62,3 +62,6 @@ class Login(LoginView):
             return reverse_lazy('account:home')
         else:
             return reverse_lazy('account:profile')
+
+class PasswordChange(PasswordChangeView):
+	success_url = reverse_lazy("account:password_change_done")
