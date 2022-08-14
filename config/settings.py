3fda7a9d7bf06d8cb3ad5b745e1d108250d26aa5
irania90 <wwwwwq37@gmail.com>
2022-08-14 18:54:52 +0430

    add star-ratings articles.

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_gravatar',
     'comment',
+    'star_ratings',
     'extentions'
 ]
 
@@ -154,3 +155,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER =config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+STAR_RATINGS_STAR_HEIGHT = 20
--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,50})/$',activate, name='activate'),
     path('admin/', admin.site.urls),
     path('comment/', include('comment.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
     path('account/', include('account.urls'))
 ]
 
