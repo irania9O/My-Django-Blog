f269c5b4443fa4fc5d3bebf46c02f589b7197a88
irania90 <wwwwwq37@gmail.com>
2022-08-22 22:14:02 +0430

    update displaying author new way.

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 # from account.models import User
 from django.contrib.auth import get_user_model
 from blog.models import Article
 
 
-class AuthorSerializer(ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ["id", "username", "first_name", "last_name"]
+# class AuthorSerializer(ModelSerializer):
+#     class Meta:
+#         model = get_user_model()
+#         fields = ["id", "username", "first_name", "last_name"]
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -16,7 +16,16 @@ class UserSerializer(ModelSerializer):
 
 
 class ArticleSerializer(ModelSerializer):
-    author = AuthorSerializer()
+    # author = AuthorSerializer()
+
+    def get_author(self, obj):
+        return {
+            "pk": obj.author.pk,
+            "username": obj.author.username,
+            "first_name": obj.author.first_name,
+            "last_name": obj.author.last_name,
+        }
+    author = SerializerMethodField("get_author") 
     class Meta:
         model = Article
         fields = "__all__"
